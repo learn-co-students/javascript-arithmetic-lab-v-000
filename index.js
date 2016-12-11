4fb053c38488bb417b99344a4a5bda18ebbef158
Daniel Suarez <suarez.daniel101@gmail.com>
2016-12-11 21:29:50 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,38 @@
+var a
+var b
+var n
+
+function add(a, b) {
+  return a + b
+}
+
+function subtract(a, b) {
+  return a - b
+}
+
+function multiply(a, b) {
+  return a * b
+}
+
+function divide(a, b) {
+  return a / b
+}
+
+function inc(n) {
+  return n + 1
+}
+
+function dec(n) {
+  return n - 1
+}
+
+function makeInt(n){
+  if (n){
+  parseInt("n", 10)
+  return n
+} else if (n = string) {
+  return "NaN"
+} else if ('0 * n') {
+  return 0
+}
+}
