9b746aacd9a2fc5dd9c7c66301dc1fb9dae7d6ef
Christian Whitesides <whitesidescr@gmail.com>
2017-03-09 15:10:24 -0700

    Done.

--- index.js
+++ index.js
@@ -15,13 +15,11 @@ function divide(a, b) {
 }
 
 function inc(n) {
-  var num = n
-  return num += 1
+  return n += 1
 }
 
 function dec(n) {
-  var num = n
-  return num -= 1
+  return n -= 1
 }
 
 function makeInt(n) {
